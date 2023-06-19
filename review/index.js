4e29f0c26c45871655e61fb68d210ec359141c92
Wayne-dot <wayneting.wc@gmail.com>
2023-06-19 11:54:26 -0400

    change

--- review/index.js
+++ review/index.js
@@ -5,3 +5,14 @@
 // addEventListener()
 // array.length
 // textContent
+
+
+const forward = document.getElementById("forward")
+forward.addEventListener("click", function(){
+    console.log("forward click");
+})
+
+const backward = document.getElementById("backward")
+backward.addEventListener("click", function(){
+    console.log("backward click");
+})
