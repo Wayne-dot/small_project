ab5d22b5f4924cb35147e083fadf4a0fb7418e2b
Wayne-dot <wayneting.wc@gmail.com>
2023-06-21 15:07:17 -0400

    change

--- review/index.js
+++ review/index.js
@@ -6,10 +6,18 @@
 // array.length
 // textContent
 
+const persons = {
+    author: "James weldon",
+    job: "backend engineer",
+    info: "As a seasoned software backend engineer, I excel in designing robust architectures, optimizing performance, and implementing scalable solutions. My expertise lies in crafting efficient code that powers seamless and reliable applications.",
+}
+
 
 const forward = document.getElementById("forward")
 forward.addEventListener("click", function(){
     console.log("forward click");
+    let author = document.getElementById("author").textContent;
+    console.log(`${author}`);
 })
 
 const backward = document.getElementById("backward")
