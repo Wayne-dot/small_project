eb687a5226c52b42086ca849d995a0dad39708e5
Wayne-dot <wayneting.wc@gmail.com>
2023-06-29 09:24:07 -0400

    chnage

--- review/index.js
+++ review/index.js
@@ -6,11 +6,18 @@
 // array.length
 // textContent
 
-const persons = {
+const persons1 = {
     author: "James weldon",
     job: "backend engineer",
     info: "As a seasoned software backend engineer, I excel in designing robust architectures, optimizing performance, and implementing scalable solutions. My expertise lies in crafting efficient code that powers seamless and reliable applications.",
 }
+
+const persons2 = {
+    author: "Adin Johnson",
+    job: "frontend engineer",
+    info: "My job title is junior software backend engineer and I know C++ and C#",
+}
+
 
 
 const forward = document.getElementById("forward")
